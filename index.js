15ca761a948f08a2c9cf15cbad886482471acfcc
Eswar Saladi <eswar.saladi@ateliasoftware.com>
2022-09-29 09:50:23 -0400

    added initial server setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+app.use(morgan("combined"));
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/healthz", (req, res) =>
+  res.status(200).json({ message: "Server is Live!!!" })
+);
+
+app.listen(PORT, () => console.log(`Web App listening on port ${PORT}!`));
