991a6e084364d8c4b12277945604944d44b95447
eswarsaladi <99567868+eswarsaladi@users.noreply.github.com>
2022-10-06 15:32:09 -0400

    A2 01 (#6)
    * added details about super test
    
    * modified routes to be more informative and tested
    
    * added env variables
    
    Co-authored-by: Eswar Saladi <saladi.e@northeastern.edu>

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,16 @@
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+
 module.exports = {
   development: {
     client: "pg",
     connection: {
-      database: "webapp",
-      user: "eswar",
-      host: "localhost",
-      port: 5432,
+      database: DB_NAME,
+      user: DB_USER,
+      host: DB_HOST,
+      port: DB_PORT,
     },
     migrations: {
       directory: __dirname + "/db/migrations",
